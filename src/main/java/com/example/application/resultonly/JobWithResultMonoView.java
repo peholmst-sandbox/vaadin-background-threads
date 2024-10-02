336aae7725be78ca76b360956b26cef171cb8af8
Petter Holmström <petter@vaadin.com>
2024-10-02 15:23:03 +0300

    Add Mono version of JobWithResult

---
+++ src/main/java/com/example/application/resultonly/JobWithResultMonoView.java
@@ -0,0 +1,48 @@
+package com.example.application.resultonly;
+
+import com.example.application.utils.UIAwareConsumer;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.progressbar.ProgressBar;
+import com.vaadin.flow.router.Route;
+
+@Route("job-with-result-mono")
+@SuppressWarnings("DuplicatedCode")
+public class JobWithResultMonoView extends VerticalLayout {
+
+    private final Button start;
+    private final ProgressBar progressBar;
+
+    public JobWithResultMonoView(JobWithResultTrigger trigger) {
+        setWidth("400px");
+        progressBar = new ProgressBar();
+        progressBar.setIndeterminate(true);
+        progressBar.setVisible(false);
+        start = new Button("Start job", event -> {
+            progressBar.setVisible(true);
+            trigger.runJobAsMono()
+                    .doOnError(new UIAwareConsumer<>(this::onJobFailed))
+                    .subscribe(new UIAwareConsumer<>(this::onJobCompleted));
+        });
+        start.setDisableOnClick(true);
+        add(progressBar, start);
+    }
+
+    private void onJobCompleted(String output) {
+        Notification.show("Job completed: " + output, 3000, Notification.Position.MIDDLE);
+        start.setEnabled(true);
+        progressBar.setVisible(false);
+    }
+
+    private void onJobFailed(Throwable error) {
+        var notification = new Notification("Job failed with error: " + error.getMessage());
+        notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
+        notification.setPosition(Notification.Position.MIDDLE);
+        notification.setDuration(3000);
+        notification.open();
+        start.setEnabled(true);
+        progressBar.setVisible(false);
+    }
+}
--- src/main/java/com/example/application/resultonly/JobWithResultTrigger.java
+++ src/main/java/com/example/application/resultonly/JobWithResultTrigger.java
@@ -1,7 +1,10 @@
 package com.example.application.resultonly;
 
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -9,13 +12,19 @@ import java.util.concurrent.CompletableFuture;
 public class JobWithResultTrigger {
 
     private final JobWithResult job;
+    private final TaskExecutor taskExecutor;
 
-    public JobWithResultTrigger(JobWithResult job) {
+    public JobWithResultTrigger(JobWithResult job, TaskExecutor taskExecutor) {
         this.job = job;
+        this.taskExecutor = taskExecutor;
     }
 
     @Async
     public CompletableFuture<String> runJob() {
         return CompletableFuture.completedFuture(job.startLongRunningJob());
+    }
+
+    public Mono<String> runJobAsMono() {
+        return Mono.fromSupplier(job::startLongRunningJob).subscribeOn(Schedulers.fromExecutor(taskExecutor));
     }
 }
